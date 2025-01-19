6e1785688007e3b088baf050f33660e12675a461
Eman Gaber <emangaber413@gmail.com>
2025-01-19 20:09:36 +0200

    final2

--- fristGame.js
+++ fristGame.js
@@ -1,35 +1,72 @@
 
 let testimages = [];
-document.addEventListener("DOMContentLoaded", function () {
+let scoreRate=0
+let Game=document.querySelector(".game")
+
   let images = document.querySelectorAll("img");
   
-  function show() {
-    images.classList.add("opacity-100");
-  }
-
-  
-        
-            images.forEach((image) => {
-            image.addEventListener("click",function () {
-               
-            testimages.push(image);
-      
-      
-
-      if (testimages[0].getAttribute("role") === testimages[1].getAttribute("role")  ) {
-        console.log(testimages );
+  images.forEach((image) => {
+    image.addEventListener("click",function () {
+      if(testimages.length<2){
        
-          testimages.forEach((item) => {
-            item.classList.add("success");
-            testimages = [];
-          });
+    image.classList.add("opacity-100");
+    testimages.push(image); 
+
+  if (  testimages[0].getAttribute("role") === testimages[1].getAttribute("role") && scoreRate<16)  {
+
+        testimages.forEach((selectedItem) => {
+       
+          selectedItem.classList.add("success");
+          scoreRate++
           
-        }
+
+          let score =document.querySelector(".score")
+          let htmltext=`<h2 class=" pe-1 pe-md-3 ">score:${scoreRate/2}</h2>
+                         <h2 class="pe-1 pe-md-3 fw-bolder text-capitalize "> winner</h2>`
+                
+             score.innerHTML=htmltext
+          testimages = [];
+          console.log(scoreRate)
+
+          if (scoreRate===16) { 
+          document.querySelector(".finishGame").classList.add("finish")
+          Game.classList.add("z-n1")
+
+          }
+         
+        });
         testimages = [];
-  
-});
+      }
+    }
+     
+      else{ testimages.forEach ((selectedItem)=>{selectedItem.classList.remove("opacity-100");})
+    testimages=[]}
+  })
+
+
 })
-})
+        
+      
+      
+
+      // if (testimages[0].getAttribute("role") === testimages[1].getAttribute("role") )  {
+
+      //   const top1 = getComputedStyle(testimages[0]).top
+      //   console.log(testimages);
+      //   console.log(top1);
+        
+         
+      //     testimages.forEach((item) => {
+         
+      //       item.classList.add("success");
+      //       testimages = [];
+      //     });
+        
+      //   }
+      //   testimages = [];
+
+
+
   
 
 
